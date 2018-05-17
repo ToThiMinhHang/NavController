da89bac74052da46fdfa1abcce648847deba1442
To Hang <minhhangbn1997@gmail.com>
2018-05-17 21:35:56 +0700

    NavCotroller

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AddPage } from './../add/add';
+import { PersonalPage } from './../personal/personal';
+import { NoticesPage } from './../notices/notices';
+import { GroupPage } from './../group/group';
+
 
 @Component({
   selector: 'page-home',
@@ -10,5 +15,19 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  onNavControlltoAdd(){
+    this.navCtrl.push(AddPage);
+  }
+  onNavControlltoPresonal(){
+    this.navCtrl.push(PersonalPage);
+  }
+  onNavControlltoNotices() {
+  this.navCtrl.push( NoticesPage);
+  }
+  onNavControlltoGroups() {
+  this.navCtrl.push(GroupPage);
+  }
+  onNavControlltoHome() {
+  this.navCtrl.push(HomePage);
+  }
 }
